16464c8ffa4dc2fd4bed3eca3079286a4758e0bd
crocochon <chanakan.3869@gmail.com>
2022-11-05 15:47:43 +0700

    Update GameScene.js

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -3,6 +3,10 @@ import Phaser from "phaser";
 let background; //create variable for background
 let ermine; //create variable for spritesheet
 let bullet; //create variable for group object
+let basketball;
+let football;
+let volleyball;
+let bird;
 
 class GameScene extends Phaser.Scene {
     constructor(test) {
@@ -17,45 +21,67 @@ class GameScene extends Phaser.Scene {
         this.load.spritesheet('ermine','src/image/objectClass/playerErmine.png', 
         {frameWidth: 500, frameHeight: 300}); //spritesheet
         this.load.image('bullet','src/image/objectClass/Bullet.png'); 
+
+        this.load.image('basketball','src/image/objectClass/basketball.png');
+        this.load.image('football','src/image/objectClass/football.png');
+        this.load.image('volleyball','src/image/objectClass/volleyball.png');
+
+        this.load.spritesheet('bird','src/image/objectClass/bird.png',
+        {frameWidth: 410, frameHeight: 310});        
         // test
     }
 
     create() {
         background = this.add.image(100,350,'bg'); //image
-        ermine = this.physics.add.sprite(220,400,'ermine').setScale(0.5); //sprite
-        // background = this.add.tileSprite(0, 0, 450, 720, 'bg').setOrigin(0, 0); //tileSprtie
-        // bullet = this.physics.add.staticGroup(); //GroupObject
-        // bullet.create(200,300,'bullet')
-        // bullet.create(500,300,'bullet')
-        // bullet.create(400,500,'bullet')
+
+        ermine = this.add.sprite(220,400,'ermine').setScale(0.5).setDepth(2)/*.setSize(420,185).setOffset(60,55)*/; //sprite
+        this.anims.create({
+            key: 'ermineAni',
+            frames: this.anims.generateFrameNumbers('ermine', {
+                start: 0,
+                end: 2
+            }),  
+            duration: 500,
+            repeat: -1
+        })
+        //ermine.setCollideWorldBounds(true);
+
+
+        background = this.add.tileSprite(0, 0, 450, 720, 'bg').setOrigin(0, 0); //tileSprtie
+        //bullet = this.physics.add.staticGroup(); //GroupObject
+        //bullet.create(200,300,'bullet')
+        //bullet.create(500,300,'bullet')
+        //bullet.create(400,500,'bullet')
+        
+        //basketball = this.add.image(200,300,'basketball').setScale(0.1).setDepth(3);
+        //football = this.add.image(250,300,'football').setScale(0.1).setDepth(1);
+        //volleyball = this.add.image(250,350,'volleyball').setScale(0.2).setDepth(2);
+
+        /*bird = this.add.sprite(220,400,'bird').setScale(0.5)
+        this.anims.create({
+            key: 'birdAni',
+            frames: this.anims.generateFrameNumbers('bird', {
+            start: 0,
+            end: 7
+            }),
+            duration: 500,
+            repeat: -1
+        })*/
+
+        //ลองทำตัว ermine
+
     }
 
     update(delta, time) {
-        // background.tilePositionX += 2; //tileSprite
-     
-
-        //this.load.image('background','/src/image/objectClass/backGround.png');
-        this.load.spritesheet('ermine','/src/image/objectClass/playerErmine.png', {frameWidth: 500, frameHeight:300});
-        //this.load.image('bullet','src/image/objectClass/Bullet,png');
-
+    background.tilePositionX += 2; //tileSprite
+        
+       //bird.anims.play('birdAni', true);
+       ermine.anims.play('ermineAni', true);
+       
+        
     }
 
-    create() {
-        //background=this.add.image(450,720,'bg')
-        ermine = this.add.sprite(220,400,'ermine').setscale(0.5);
-       // background = this.add.tileSprite(0, 0, 450, 720, 'bg').setOrigin(0, 0);
-       //bullet = this.physics.add.staticGroup();
-       //bullet.create(200,300,'bullet')
-       //bullet.create(500,300,'bullet')
-       //bullet.create(400,500,'bullet')
-
-    }
-
-    update(delta, time) {
-        // code here
-        background.tilePositionX += 2;
-
-    }
+   
 }
 export default GameScene;
 
