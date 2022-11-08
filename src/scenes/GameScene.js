d2120628da15c10b1d9a0bfc9dee9e12458162e7
panithee <panithee08@gmail.com>
2022-11-08 16:22:55 +0700

    test input

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -6,6 +6,7 @@ let basketball;
 let football;
 let volleyball;
 let bird;
+let cursor;
 
 class GameScene extends Phaser.Scene {
     constructor(test) {
@@ -29,19 +30,34 @@ class GameScene extends Phaser.Scene {
     }
 
     create() {
-        // background = this.add.image(100,350,'bg'); //image
+        background = this.add.image(600,360,'bg'); //image
         // ermine = this.physics.add.sprite(220,400,'ermine').setScale(0.5).setDepth(2).setSize(420,200).setOffset(60,70); //sprite
-        // ermine.setCollideWorldBounds(true);
+        
         // background = this.add.tileSprite(0, 0, 450, 720, 'bg').setOrigin(0, 0).setDepth(1); //tileSprtie
-        // bullet = this.physics.add.staticGroup(); //GroupObject
-        // bullet.create(200,300,'bullet')
-        // bullet.create(500,300,'bullet')
-        // bullet.create(400,500,'bullet')
 
-        // basketball = this.add.image(200,300,'basketball').setScale(0.1).setDepth(3);
-        // football = this.add.image(250,300,'football').setScale(0.1).setDepth(1);
-        // volleyball = this.add.image(250,350,'volleyball').setScale(0.2).setDepth(2);
-        bird = this.add.sprite(220,400,'bird').setScale(0.5)
+        // this.input.on('pointermove', (pointer)=>{
+        //     ermine.x = pointer.x
+        //     ermine.y = pointer.y
+
+        // })
+
+       
+        cursor = this.input.keyboard.createCursorKeys();
+        ermine = this.physics.add.sprite(220,400,'ermine').setScale(0.5).setDepth(2).setSize(420,200).setOffset(60,70).setX(700).setY(500)
+       
+        this.anims.create({
+           key: 'ermineAni',
+           frames: this.anims.generateFrameNumbers('ermine', {
+            start: 0,
+            end: 2
+           }),
+           duration: 500,
+           repeat: -1  
+        })
+
+    
+
+        bird = this.physics.add.sprite(220,400,'bird').setScale(0.5)
         this.anims.create({
            key: 'birdAni',
            frames: this.anims.generateFrameNumbers('bird', {
@@ -51,11 +67,43 @@ class GameScene extends Phaser.Scene {
            duration: 500,
            repeat: -1  
         })
+
+
+        
+        this.physics.add.collider(ermine, bird)
+        // ermine.setCollideWorldBounds(true);
+        // bird.setImmovable()
     }
 
     update(delta, time) {
-        // background.tilePositionX += 2; //tileSprite
-        bird.anims.play('birdAni', true);
+        if(!cursor.isDown){
+            if(cursor.up.isDown){
+                ermine.setVelocityY(-300);
+                ermine.anims.play('ermineAni', true)
+            }else if(cursor.down.isDown){
+                ermine.setVelocityY(300);
+                ermine.anims.play('ermineAni', true)
+               
+            }
+            else if(cursor.left.isDown){
+                ermine.setVelocityX(-300);
+                ermine.anims.play('ermineAni', true)
+              
+             }else if(cursor.right.isDown){
+                 ermine.setVelocityX(300);
+                 ermine.anims.play('ermineAni', true)
+                 
+             }
+             else{
+                ermine.setVelocityY(0);
+                ermine.setVelocityX(0);
+                ermine.anims.play('ermineAni', false)
+            }
+           
+        }
+        
+        
+        
     }
 }
 export default GameScene;
