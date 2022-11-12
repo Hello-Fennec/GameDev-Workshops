75293b39a74bc520c6719e55a8a714d31745bb02
Armmys <tankm3310@gmail.com>
2022-11-12 15:49:42 +0700

    12/11/2565

--- src/main.js
+++ src/main.js
@@ -2,13 +2,14 @@ import 'phaser';
 import Phaser from 'phaser';
 import GameScene from './scenes/GameScene';
 import MainMenu from './scenes/MainMenu';
+import EventScene from './scenes/EventScene';
 const config = {
     // For more settings see <https://github.com/photonstorm/phaser/blob/master/src/boot/Config.js>
     type: Phaser.WEBGL,
     pixelArt: true,
     roundPixels: true,
     parent: 'content',
-    width: 1080,
+    width: 450,
     height: 720,
     physics: {
         default: 'arcade',
@@ -17,6 +18,7 @@ const config = {
         }
     },
     scene: [
+        EventScene,
         MainMenu,
         GameScene
     ]
