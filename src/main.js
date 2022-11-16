069b6f288195653241305ec1ad17049bd136f461
Sirapob <blacking987@gmail.com>
2022-11-16 18:59:48 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,27 +1,26 @@
-import 'phaser';
-import Phaser from 'phaser';
-import GameScene from './scenes/GameScene';
+import "phaser";
+import Phaser from "phaser";
+import GameScene from "./scenes/GameScene";
+import MainMenu from "./scenes/MainMenu";
+import CutScene from "./scenes/CutScene";
+import EventScene from "./scenes/EventScene";
+import Bullet from "./scenes/Bullet";
 
 const config = {
     // For more settings see <https://github.com/photonstorm/phaser/blob/master/src/boot/Config.js>
     type: Phaser.WEBGL,
     pixelArt: true,
     roundPixels: true,
-    parent: 'content',
-    width: 450,
+    parent: "content",
+    width: 1080,
     height: 700,
     physics: {
-        default: 'arcade',
+        default: "arcade",
         arcade: {
-            debug:false
-        }
+            debug: false,
+        },
     },
-    scene: [
-        GameScene
-    ],
-    
-    
+    scene: [EventScene, Bullet, GameScene, MainMenu, CutScene],
 };
 
 const game = new Phaser.Game(config);
-
