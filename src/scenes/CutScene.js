d6749ac1ef1a38d7b8466b2eae9eae7ec90498f1
Sirapob <blacking987@gmail.com>
2022-11-18 23:24:48 +0700

    Create CutScene.js

---
+++ src/scenes/CutScene.js
@@ -0,0 +1,16 @@
+import Phaser from "phaser";
+
+class CutScene extends Phaser.Scene {
+    constructor(test) {
+        super({
+            key: "CutScene",
+        });
+    }
+
+    preload() {}
+
+    create() {}
+
+    update(delta, time) {}
+}
+export default CutScene;
